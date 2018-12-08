a5ba214af9c53e8fbcc2a7cb85323d37743954df
JayantiU <33074861+JayantiU@users.noreply.github.com>
2018-12-08 11:07:39 -0500

    Add files via upload

---
+++ BHC/main.py
@@ -0,0 +1,19 @@
+#Border city hacks program
+#Game; fruit ninja
+from pygame import *
+from os import environ 
+#---Screen Setup---#
+init()
+inf = display.Info()
+cen=(inf.current_w-1200)/2 #finds the x coordinate so that the screen can be centered (this is how much distance there'll be from the left and right corner of computer screen to the left and right corner (respectively)of the paint program screen)
+environ['SDL_VIDEO_WINDOW_POS'] = '%d,25'%cen 
+screen=display.set_mode((800,600))
+display.set_caption('Food Ninja')
+display.set_icon(image.load('logo.jpg'))
+
+
+
+
+#---Displaying---#
+display.flip()
+
